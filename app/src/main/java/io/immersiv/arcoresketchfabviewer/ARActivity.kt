bea581ed2ee95b610edcbb18305cbf4e8ff3c163
Stéphane Guérin <sg@immersiv.io>
2018-11-18 11:13:57 +0100

    added model loaded at runtime

--- app/src/main/java/io/immersiv/arcoresketchfabviewer/ARActivity.kt
+++ app/src/main/java/io/immersiv/arcoresketchfabviewer/ARActivity.kt
@@ -1,13 +1,22 @@
 package io.immersiv.arcoresketchfabviewer
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.assets.RenderableSource
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+
 
 class ARActivity : AppCompatActivity() {
 
     private var arFragment: ArFragment? = null
+    private var duckRenderable: ModelRenderable? = null
+    private val GLTF_ASSET = "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +25,43 @@ class ARActivity : AppCompatActivity() {
         arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
 
 
+        ModelRenderable.builder()
+            .setSource(
+                this, RenderableSource.builder().setSource(
+                    this,
+                    Uri.parse(GLTF_ASSET),
+                    RenderableSource.SourceType.GLTF2
+                )
+                    .setScale(0.5f)  // Scale the original model to 50%.
+                    .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                    .build()
+            )
+            .setRegistryId(GLTF_ASSET)
+            .build()
+            .thenAccept { renderable -> duckRenderable = renderable }
+            .exceptionally { throwable ->
+                val toast = Toast.makeText(this, "Unable to load renderable $GLTF_ASSET", Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.CENTER, 0, 0)
+                toast.show()
+                null
+            }
+
+
         arFragment?.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
-            Toast.makeText(this@ARActivity, "Coucou", Toast.LENGTH_LONG).show()
+            if (duckRenderable == null) {
+                return@setOnTapArPlaneListener
+            }
+
+            // Create the Anchor.
+            val anchor = hitResult.createAnchor()
+            val anchorNode = AnchorNode(anchor)
+            anchorNode.setParent(arFragment?.getArSceneView()?.scene)
+
+            // Create the transformable andy and add it to the anchor.
+            val andy = TransformableNode(arFragment?.transformationSystem)
+            andy.setParent(anchorNode)
+            andy.renderable = duckRenderable
+            andy.select()
         }
     }
 }
