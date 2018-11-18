b5dd3ab68e86a999b1cb52ff8c2f54194e36a07d
Stéphane Guérin <sg@immersiv.io>
2018-11-18 10:59:09 +0100

    added LoginActivity

---
+++ app/src/main/java/io/immersiv/arcoresketchfabviewer/LoginActivity.kt
@@ -0,0 +1,44 @@
+package io.immersiv.arcoresketchfabviewer
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.loader.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        contentLayout.visibility = View.GONE
+        progressLayout.visibility = View.VISIBLE
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setTitle(R.string.sketchfab_login)
+
+        contentLayout.webViewClient = CustomWebViewClient()
+        contentLayout.loadUrl("https://sketchfab.com")
+    }
+
+    inner class CustomWebViewClient : WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            contentLayout.visibility = View.VISIBLE
+            progressLayout.visibility = View.GONE
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- app/src/main/java/io/immersiv/arcoresketchfabviewer/MainActivity.kt
+++ app/src/main/java/io/immersiv/arcoresketchfabviewer/MainActivity.kt
@@ -1,12 +1,15 @@
 package io.immersiv.arcoresketchfabviewer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        login.setOnClickListener { startActivity(Intent(this@MainActivity, LoginActivity::class.java)) }
     }
 }
